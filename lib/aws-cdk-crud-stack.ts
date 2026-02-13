7f2867debd8ee23a8215547247c5c3b2a7cce714
Manikantha M <manikantha.m@neutrinos.co>
2026-02-13 15:27:37 +0300

    dynamo db stack

--- lib/aws-cdk-crud-stack.ts
+++ lib/aws-cdk-crud-stack.ts
@@ -6,16 +6,24 @@ import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import path from 'path';
 import * as apigateway from 'aws-cdk-lib/aws-apigatewayv2';
 import * as apigateway_integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { DynamoDBStack } from './dynamodb-stack';
 
+interface UsersApiStackProps extends cdk.StackProps {
+  dynamodbStack:DynamoDBStack
+}
 export class AwsCdkCrudStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: UsersApiStackProps) {
     super(scope, id, props);
     const userHandler = new NodejsFunction(this, 'UserHandler', {
       runtime:Runtime.NODEJS_22_X,
       entry: path.join(__dirname, '../src/lambda/handler.ts'),
       handler: 'handler',
-      functionName: `${this.stackName}-user-handler`
+      functionName: `${this.stackName}-user-handler`,
+      environment: {
+        TABLE_NAME: props.dynamodbStack.usersTable.tableName
+      }
     });
+    props.dynamodbStack.usersTable.grantReadWriteData(userHandler);
     const httpApi = new apigateway.HttpApi(this, 'UserApi', {
       apiName: 'Users API',
       description: 'Users Management API',
