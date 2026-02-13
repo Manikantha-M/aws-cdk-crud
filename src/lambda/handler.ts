330b362a3d9637848572553522f11db8a98d7677
Manikantha M <manikantha.m@neutrinos.co>
2026-02-13 16:36:29 +0300

    crud operation tested

--- src/lambda/handler.ts
+++ src/lambda/handler.ts
@@ -1,6 +1,14 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand, GetCommand, UpdateCommand, DeleteCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { v4 as uuidv4 } from 'uuid';
+import { faker } from '@faker-js/faker';
 
-export const handler = async (event:APIGatewayProxyEventV2):Promise<APIGatewayProxyResult> => {
+const client = new DynamoDBClient({});
+const dynamoDB = DynamoDBDocumentClient.from(client);
+const TABLE_NAME = process.env.TABLE_NAME || '';
+
+export const handler = async (event:APIGatewayProxyEventV2):Promise<APIGatewayProxyResultV2> => {
     const method = event.requestContext.http.method;
     const path = event.requestContext.http.path;
     try {
@@ -56,35 +64,84 @@ export const handler = async (event:APIGatewayProxyEventV2):Promise<APIGatewayPr
 
 
 
-async function getAllUser(event:APIGatewayProxyEventV2):Promise<APIGatewayProxyResult> {
+async function getAllUser(event:APIGatewayProxyEventV2):Promise<APIGatewayProxyResultV2> {
+    const result = await dynamoDB.send(
+        new ScanCommand({
+            TableName:TABLE_NAME
+        })
+    )
     return {
         statusCode: 200,
-        body: JSON.stringify({message: 'fetch all users'})
+        body: JSON.stringify({message: result.Items})
     }
 };
 
-async function createUser(event:APIGatewayProxyEventV2):Promise<APIGatewayProxyResult> {
+async function createUser(event:APIGatewayProxyEventV2):Promise<APIGatewayProxyResultV2> {
+    // const {name, email} = JSON.parse(event.body!);
+    const userId = uuidv4();
+    const user = {
+        id:userId,
+        name:faker.person.fullName(),
+        email:faker.internet.email(),
+        createdAt: new Date().toISOString()
+    }
+    await dynamoDB.send(new PutCommand({
+        TableName: TABLE_NAME,
+        Item:user
+    }
+    ))
     return {
         statusCode: 201,
-        body: JSON.stringify({message: 'create user'})
+        body: JSON.stringify({message: user})
     }
 };
 
-async function getUser(userId:string):Promise<APIGatewayProxyResult> {
+async function getUser(userId:string):Promise<APIGatewayProxyResultV2> {
+    const result = await dynamoDB.send(new GetCommand({
+        TableName:TABLE_NAME,
+        Key: {id:userId}
+    }));
+    if(!result.Item){
+    return {
+        statusCode: 404,
+        body: JSON.stringify({message: 'user not found'})
+    } 
+    }
     return {
         statusCode: 200,
-        body: JSON.stringify({message: 'fetch single user'})
+        body: JSON.stringify({message: result.Item})
     }
 };
 
-async function updateUser(event:APIGatewayProxyEventV2, userId:string):Promise<APIGatewayProxyResult> {
+async function updateUser(event:APIGatewayProxyEventV2, userId:string):Promise<APIGatewayProxyResultV2> {
+    const {name, email} = JSON.parse(event.body!);
+    const result = await dynamoDB.send(
+        new UpdateCommand({
+            TableName:TABLE_NAME,
+            Key:{id:userId},
+            UpdateExpression:`SET #name = :name, #email = :email`,
+            ExpressionAttributeNames: {
+                '#name': 'name',
+                '#email': 'email'
+            },
+            ExpressionAttributeValues: {
+                ':name': name || null,
+                ':email': email || null
+            },
+            ReturnValues:'ALL_NEW'
+        })
+    )
     return {
         statusCode: 200,
-        body: JSON.stringify({message: 'update user'})
+        body: JSON.stringify({message: result.Attributes})
     }
 };
 
-async function deleteUser(userId:string):Promise<APIGatewayProxyResult> {
+async function deleteUser(userId:string):Promise<APIGatewayProxyResultV2> {
+    await dynamoDB.send(new DeleteCommand({
+        TableName:TABLE_NAME,
+        Key: {id:userId}
+    }))
     return {
         statusCode: 200,
         body: JSON.stringify({message: `deleted user: ${userId}`})
